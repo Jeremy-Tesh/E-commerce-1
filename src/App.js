87afcf86b6857e7fea37dddea1c7294f4e2b3462
Henok8515 <henokegezew44@gmail.com>
2022-03-02 19:37:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
+import React from 'react';
 import Header from './components/Header/Header';
-const Data = [
-    {
-        name: 'henok',
-        id: 1
-    },
-    {
-        name: 'alex',
-        id: 2
-    },
-    {
-        name: 'nati',
-        id: 3
-    },
-    {
-        name: 'selam',
-        id: 4
-    }
-];
-
 function App() {
     return (
         <div>
-            {Data.map((name) => {
-                return (
-                    <div key={name.id}>
-                        <p onClick={() => console.log(name.id)}>{name.name}</p>
-                    </div>
-                );
-            })}
+            <Header />
         </div>
     );
 }
