435521a09db99adf46320b1d0fd4c49e59c55abe
Henok8515 <henokegezew44@gmail.com>
2022-03-02 20:26:03 +0300

    practicing merging

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Shop from './Pages/ShopPage/Shop';
 function App() {
     return (
         <div>
             <Header />
+            <Shop />
         </div>
     );
 }
---
+++ src/Pages/ShopPage/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Shop() {
+    return (
+        <div>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+        </div>
+    );
+}
+
+export default Shop;
