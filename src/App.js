330a2b6f13655559bd8cc539360f227b23c63549
Jeremy Tesh <jermitesh20@gmail.com>
2022-03-05 21:52:22 +0300

    home page on progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './Pages/HomePage/Home';
+
 function App() {
     return (
         <div>
-            <Header />
+            <Home />
         </div>
     );
 }
