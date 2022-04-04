e1fb23e7ddfbb64767846745f63f2583d548d359
henok8515 <henokegezew44@gmail.com>
2022-04-04 14:04:45 +0300

    fixing a bug

--- src/Pages/HomePage/Section5/Section5.js
+++ src/Pages/HomePage/Section5/Section5.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import image1 from '../../../Assets/Images/product1.jpg';
+import image2 from '../../../Assets/Images/product2.jpg';
 
 function Section5() {
-  return (
-    <>
-    
-    </>
-  )
+    return (
+        <>
+            <div className="flex ...">
+                <div className="col-1">
+                    <div className="flex flex-col ...">
+                        <p>SPECIAL</p>
+                        <h1>Refine Your Style</h1>
+                        <p>
+                            Get on our exclusive emial list and be the first to
+                            hear
+                        </p>
+                        <br></br>
+                        <p>about sales,coupones,new arrivals and more!!</p>
+                    </div>
+                    <div className="flex ...">
+                        <div className="flex flex-col ...">
+                            <img className="w-40" src={image1} alt="" />
+                            <span>Boots,Shoes,Women</span>
+                            <span>Sandals Cool</span>
+                            <span>Now $337.00</span>
+                            <span>Now $337.00</span>
+                        </div>
+                        <div className="flex flex-col ...">
+                            <img className="w-40" src={image2} alt="" />
+                            <span>Boots,Shoes,Women</span>
+                            <span>Sandals Cool</span>
+                            <span>Now $337.00</span>
+                            <span>Now $337.00</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Section5
+export default Section5;
