74e47c3433e3061a70033f90c77aa5e3e6d93ee4
Jeremy <59338167+Jeremy-Tesh@users.noreply.github.com>
2022-04-05 22:58:29 +0300

    Update Section1.js

--- src/Pages/HomePage/Section1/Section1.js
+++ src/Pages/HomePage/Section1/Section1.js
@@ -6,16 +6,14 @@ import Image4 from '../../../Assets/Images/3.png';
 import Image5 from '../../../Assets/Images/4.png';
 import Image6 from '../../../Assets/Images/5.png';
 import Image7 from '../../../Assets/Images/6.png';
-import { Container, BrandLogo } from './style';
+import { Container } from './style';
 
-const BrandLogos = [Image2, Image3, Image4, Image5, Image6, Image7];
+const brandLogos = [Image2, Image3, Image4, Image5, Image6, Image7];
 
 function Section1() {
     return (
         <>
-
             <Container className="flex flex-col justify-center items-center text-white -mt-20 ">
-
                 <div
                     className="motto-text"
                     style={{ color: 'white', textAlign: 'center' }}
@@ -56,21 +54,24 @@ function Section1() {
                     </Button>
                 </div>
             </Container>
-            <BrandLogo>
-                {BrandLogos.map((logo) => (
+            <div className="flex justify-center overflow-auto hover:overflow-scroll">
+                {brandLogos.map((logo, id) => (
                     <div
-                        className="brand-images-row"
+                        className="brand-images-row "
+                        key={id}
                         style={{
                             display: 'flex',
                             justifyContent: 'center',
                             alignItems: 'center',
-                            height: '100px'
+                            minWidth: '271px',
+                            height: '99px',
+                            border: 'solid grey 1px'
                         }}
                     >
                         <img src={logo} alt="image1" />
                     </div>
                 ))}
-            </BrandLogo>
+            </div>
         </>
     );
 }
