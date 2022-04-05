07db0d1c8156398a560682551a23e538b1112513
Jeremy <59338167+Jeremy-Tesh@users.noreply.github.com>
2022-04-05 22:49:51 +0300

    Update style.js

--- src/Pages/HomePage/Section1/style.js
+++ src/Pages/HomePage/Section1/style.js
@@ -4,7 +4,7 @@ import img from '../../../Assets/Images/slide-1.jpg';
 export const Container = styled.div`
     top: -70px;
 
-    /* background-image: url(${img}); */
+    background-image: url(${img}); 
     background-repeat: no-repeat;
     background-size: cover;
     background-position: 50%;
